60090253acd0582f0448961532c0a239f77e37c1
uncjackg <jackg@live.unc.edu>
2024-01-20 12:54:43 -0500

    scrape game logs

--- scrape_active_players.py
+++ scrape_active_players.py
@@ -2,6 +2,12 @@ from selenium.webdriver.common.by import By
 from utils import init_web_driver
 import pandas as pd
 import string
+import re
+
+def create_br_url_id(player_name):
+  player_name = player_name.lower().split(' ')
+  last_name_id = player_name[1][:5] if len(player_name[1]) > 4 else player_name[1]
+  return player_name[1][0] + '/' + last_name_id + player_name[0][:2]
 
 driver = init_web_driver()
 
@@ -34,6 +40,24 @@ for letter in lowercase_letters:
         rookie_year = elem
         is_active_flag = True
 
-# store active_players as series
+# store active_players as a DataFrame
 active_players_df = pd.DataFrame(active_players, columns=['Name', 'Rookie Year'])
-active_players_df.to_csv('active_players.csv')
+
+# generate player ID's for basketball-reference scraping
+active_players_df['Basketball-Reference URL ID'] = active_players_df['Name'].apply(lambda x: create_br_url_id(x))
+
+# for non-unique name ids, enumerate like B-R does (e.g. Mikal Bridges and Miles Bridges, both "bridgmi")
+non_unique_ids = active_players_df['Basketball-Reference URL ID'][active_players_df['Basketball-Reference URL ID'].duplicated(keep=False)].unique()
+for value in non_unique_ids:
+  mask = active_players_df['Basketball-Reference URL ID'] == value
+  for i, index in enumerate(active_players_df[mask].index, start=1):
+    active_players_df.at[index, 'Basketball-Reference URL ID'] = f"{active_players_df.at[index, 'Basketball-Reference URL ID']}{i:02d}"
+
+# for all other name ids, add "01" in accordance with B-R ID system
+for index, row in active_players_df.iterrows():
+  if not bool(re.search(r'\d$', row['Basketball-Reference URL ID'])):
+      active_players_df.at[index, 'Basketball-Reference URL ID'] += '01'
+
+# drop dupes and save updated list of active players
+active_players_df.drop_duplicates(inplace=True)
+active_players_df.to_csv('active_players.csv', index=False)
--- scrape_player_game_logs.py
+++ scrape_player_game_logs.py
@@ -3,37 +3,72 @@ from utils import init_web_driver, patient_click
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
+import os
 
 GAMELOG_HEADER_TITLES = "Rk,G,Date,Age,Tm,,Opp,,GS,MP,FG,FGA,FG%,3P,3PA,3P%,FT,FTA,FT%,ORB,DRB,TRB,AST,STL,BLK,TOV,PF,PTS,GmSc,+/-"
 
-def scrape_game_log(player_name, rookie_year):
+def scrape_game_log(player_name, rookie_year, player_url_id):
   driver = init_web_driver()
   driver.implicitly_wait(3)
   
   player_name = player_name.lower().split(' ')
-  output_file_name = player_name[0] + '_' + player_name[1]
-  # TODO: account for Miles/Mikal Bridges - they have the same ID and are labeled 01/02 in the URL
-  last_name_id = player_name[1][:5] if len(player_name[1]) > 4 else player_name[1]
-  player_url_id = player_name[1][0] + '/' + last_name_id + player_name[0][:2]
-  url = 'https://www.basketball-reference.com/players/' + player_url_id + '01/gamelog/'
+  output_file_name = player_name[0] + '_' + player_name[1] + '.csv'
+  output_file_path = './player_game_logs/' + output_file_name
+  url = 'https://www.basketball-reference.com/players/' + player_url_id + '/gamelog/'
 
   for year in range(int(rookie_year), 2025):
-    driver.get(url + str(year) + '#all_pgl_basic')
-    
+    # check if this year and player have already been read (always read current season for newest games)
+    gamelog_filenames = os.listdir('./player_game_logs')
+    if output_file_name in gamelog_filenames:
+      with open(output_file_path, 'rb') as file:
+        file.seek(-2, os.SEEK_END)  # Go to the second last byte in the file
+        while file.read(1) != b'\n':  # Keep stepping back until you find the newline
+          file.seek(-2, os.SEEK_CUR)
+        most_recent_year_in_file = int(file.readline().decode().split(',')[2].split('-')[0])
+        if year != 2024 and most_recent_year_in_file >= year:
+          continue
+
     # navigate to correct section
+    driver.get(url + str(year) + '#all_pgl_basic')
     season_header_text = str(year - 1) + '-' + str(year)[2:] + ' Regular Season'
     season_header = driver.find_element(By.XPATH, '//h2[contains(text(), "{}")]/..'.format(season_header_text))
     driver.execute_script("arguments[0].scrollIntoView(true);", season_header)
     driver.execute_script("window.scrollBy(0, -50);")
+    time.sleep(2)
 
-    # turn the table into CSV format #TODO not finished yet - hover/click still wip
+    # turn the table into CSV format and grab stats
     share_export_menu = season_header.find_element(By.CLASS_NAME, 'section_heading_text')
+    time.sleep(2)
     actions = ActionChains(driver)
     actions.move_to_element(share_export_menu).perform()
     patient_click(driver.find_element(By.XPATH, '//*[contains(text(), "Get table as CSV")]'))
-    
-    # find game log elem, turn to csv table
-    # if a file doesn't exist with this player's name, create one and populate with col headers and this season
-    # if it does exist, just populate with this season's CSV, no headers
+    stats = driver.find_element(By.TAG_NAME, 'pre').text.split(GAMELOG_HEADER_TITLES)[1]
 
-scrape_game_log("Stephen Curry", '2022')
+    # if a file doesn't exist with this player's name, create one and populate with col headers and this season
+    if output_file_name not in gamelog_filenames:
+      with open(output_file_path, 'w') as file:
+        file.write(GAMELOG_HEADER_TITLES)
+        file.write(stats)
+    else:
+      with open(output_file_path, 'a') as file:
+        file.write(stats)
+
+    # drop duplicate games for current season
+    if year == 2024:
+      df = pd.read_csv(output_file_path)
+      df.drop_duplicates(inplace=True)
+      df.to_csv(output_file_path, index=False)
+
+active_players = pd.read_csv('active_players.csv')
+for index, row in active_players.iterrows():
+  name = row['Name']
+  rookie_year = row['Rookie Year']
+  br_url_id = row['Basketball-Reference URL ID']
+
+  # continually scrape until entire file has been built
+  while True:
+    try:
+      scrape_game_log(name, rookie_year, br_url_id)
+      break
+    except:
+      continue
