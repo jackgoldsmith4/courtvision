58c3b1484980b44c3c3dfd054504260c578de93a
uncjackg <jackg@live.unc.edu>
2024-01-24 08:18:00 -0500

    modify players scrape to include all historical players

---
+++ scrape_nba_players.py
@@ -0,0 +1,42 @@
+from selenium.webdriver.common.by import By
+from utils import init_web_driver
+import pandas as pd
+import string
+import time
+
+driver = init_web_driver()
+driver.implicitly_wait(2)
+
+lowercase_letters = list(string.ascii_lowercase)
+players = []
+for letter in lowercase_letters:
+  driver.get("https://www.basketball-reference.com/players/" + letter)
+  time.sleep(2)
+  players_table = driver.find_element(By.XPATH, '//*[contains(text(), "Birth Date")]/../../..')
+  players = players_table.find_elements(By.TAG_NAME, 'tr')
+  for elem in players:
+    txt = elem.text
+    if 'Player' in txt:
+      continue
+
+    player_id = letter + '/' + elem.find_element(By.TAG_NAME, 'th').get_attribute('data-append-csv')
+
+    # parse each row
+    player_name = ''
+    rookie_year = -1
+    name_final_flag = False
+    for elem in txt.split():
+      if name_final_flag:
+        players.append((player_name[:-1].replace('*', ''), player_id, rookie_year, elem, int(elem) == 2024))
+        break
+
+      if not elem.isnumeric():
+        player_name += elem + ' '
+      else:
+        rookie_year = elem
+        name_final_flag = True
+
+# store active_players as a CSV file
+players_df = pd.DataFrame(players, columns=['Name', 'Basketball-Reference URL ID', 'Rookie Year', 'Final Year', 'Active?'])
+players_df.drop_duplicates(inplace=True)
+players_df.to_csv('nba_players.csv', index=False)
