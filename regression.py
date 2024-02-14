96a42bddeffab11ea69c64a1574b22b2b5ee4a1a
uncjackg <jackg@live.unc.edu>
2024-02-14 07:53:19 -0500

    wip refactor models

--- regression.py
+++ regression.py
@@ -8,6 +8,39 @@ import pandas as pd
 import numpy as np
 import os
 
+# model 1: Linear Regression
+def linear_regression(X_train, X_train_scaled, y_train, X_test_scaled, y_test, n_train, n_test, test_mean):
+  model = LinearRegression()
+  model.fit(X_train_scaled, y_train)
+  return run_model(
+    model, 'Linreg',
+    X_train_scaled, y_train, X_test_scaled, y_test,
+    X_train.columns, n_train, n_test, test_mean
+  )
+
+# model 2: Lasso Regression
+def lasso_regression(X_train, X_train_scaled, y_train, X_test_scaled, y_test, n_train, n_test, test_mean):
+  model = Lasso()
+  model.fit(X_train_scaled, y_train)
+  return run_model(
+    model, 'Lasso',
+    X_train_scaled, y_train, X_test_scaled, y_test,
+    X_train.columns, n_train, n_test, test_mean
+  )
+
+# model 3: Random Forest Regression
+def random_forest_regression(X_train, y_train, X_test, y_test, n_train, n_test, test_mean, n_estimators, max_depth):
+  model = RandomForestRegressor(
+    n_estimators=n_estimators,
+    max_depth=max_depth,
+  )
+  model.fit(X_train, y_train)
+  return run_model(
+    model, f"Random Forest ({n_estimators} estimators, {max_depth} depth)",
+    X_train, y_train, X_test, y_test,
+    X_train.columns, n_train, n_test, test_mean
+  )
+
 def run_regressions(player_name):
   train = pd.read_csv(f"./player_game_logs/{player_name}/{player_name}_TRAIN.csv")
   test = pd.read_csv(f"./player_game_logs/{player_name}/{player_name}_TEST.csv")
@@ -33,134 +66,51 @@ def run_regressions(player_name):
   X_train_scaled = scaler.fit_transform(X_train)
   X_test_scaled = scaler.transform(X_test)
 
-  # model 1: Linear Regression
-  model = LinearRegression()
-  model.fit(X_train_scaled, y_train)
+  # run models
+  summary_df_linreg = linear_regression(X_train, X_train_scaled, y_train, X_test_scaled, y_test, n_train, n_test, test_mean)
+  summary_df_lasso = lasso_regression(X_train, X_train_scaled, y_train, X_test_scaled, y_test, n_train, n_test, test_mean)
+  summary_df_rf = random_forest_regression(X_train, y_train, X_test, y_test, n_train, n_test, test_mean, n_estimators=50, max_depth=10)
 
-  y_train_pred = model.predict(X_train_scaled)
-  mse_train = mean_squared_error(y_train, y_train_pred)
-  rmse_train = np.sqrt(mse_train)
-  r2_train = r2_score(y_train, y_train_pred)
-
-  y_pred_test = model.predict(X_test_scaled)
-  mse_test = mean_squared_error(y_test, y_pred_test)
-  rmse_test = np.sqrt(mse_test)
-  r2_test = r2_score(y_test, y_pred_test)
-
-  weights = {f"{col}_weight": coef for col, coef in zip(X_train.columns, model.coef_)}
-  bias = model.intercept_
-  
-  summary_df_linreg = pd.DataFrame({
-    'model_type': 'LinReg',
-    'n_train': n_train,
-    'n_test': n_test,
-    'mse_train': mse_train,
-    'rmse_train': rmse_train,
-    'r2_train': r2_train,
-    'mse_test': mse_test,
-    'rmse_test': rmse_test,
-    'r2_test': r2_test,
-    'y_test_mean': test_mean,
-    'bias': bias,
-  }, index=[0])
-  for key, value in weights.items():
-    summary_df_linreg[key] = value
-
-  # model 2: Lasso Regression
-  model = Lasso()
-  model.fit(X_train_scaled, y_train)
-
-  y_train_pred = model.predict(X_train_scaled)
-  mse_train = mean_squared_error(y_train, y_train_pred)
-  rmse_train = np.sqrt(mse_train)
-  r2_train = r2_score(y_train, y_train_pred)
-
-  y_pred_test = model.predict(X_test_scaled)
-  mse_test = mean_squared_error(y_test, y_pred_test)
-  rmse_test = np.sqrt(mse_test)
-  r2_test = r2_score(y_test, y_pred_test)
-
-  weights = {f"{col}_weight": coef for col, coef in zip(X_train.columns, model.coef_)}
-  bias = model.intercept_
-  
-  summary_df_lasso = pd.DataFrame({
-    'model_type': 'Lasso',
-    'n_train': n_train,
-    'n_test': n_test,
-    'mse_train': mse_train,
-    'rmse_train': rmse_train,
-    'r2_train': r2_train,
-    'mse_test': mse_test,
-    'rmse_test': rmse_test,
-    'r2_test': r2_test,
-    'y_test_mean': test_mean,
-    'bias': bias,
-  }, index=[0])
-  for key, value in weights.items():
-    summary_df_lasso[key] = value
-
-  # model 3: Random Forest Regression (100 estimators, 20 depth)
-  model = RandomForestRegressor(
-    n_estimators=100,
-    max_depth=20,
-  )
-  model.fit(X_train, y_train)
-
-  y_train_pred = model.predict(X_train)
-  mse_train = mean_squared_error(y_train, y_train_pred)
-  rmse_train = np.sqrt(mse_train)
-  r2_train = r2_score(y_train, y_train_pred)
-
-  y_pred_test = model.predict(X_test)
-  mse_test = mean_squared_error(y_test, y_pred_test)
-  rmse_test = np.sqrt(mse_test)
-  r2_test = r2_score(y_test, y_pred_test)
-  
-  summary_df_rf = pd.DataFrame({
-    'model_type': 'Random Forest (100 estimators, 20 depth)',
-    'n_train': n_train,
-    'n_test': n_test,
-    'mse_train': mse_train,
-    'rmse_train': rmse_train,
-    'r2_train': r2_train,
-    'mse_test': mse_test,
-    'rmse_test': rmse_test,
-    'r2_test': r2_test,
-    'y_test_mean': test_mean,
-  }, index=[0])
-
-  # model 4: Random Forest Regression (50 estimators, 10 depth)
-  model = RandomForestRegressor(
-    n_estimators=50,
-    max_depth=10,
-  )
-  model.fit(X_train, y_train)
-
-  y_train_pred = model.predict(X_train)
-  mse_train = mean_squared_error(y_train, y_train_pred)
-  rmse_train = np.sqrt(mse_train)
-  r2_train = r2_score(y_train, y_train_pred)
-
-  y_pred_test = model.predict(X_test)
-  mse_test = mean_squared_error(y_test, y_pred_test)
-  rmse_test = np.sqrt(mse_test)
-  r2_test = r2_score(y_test, y_pred_test)
-  
-  summary_df_rf_2 = pd.DataFrame({
-    'model_type': 'Random Forest (50 estimators, 10 depth)',
-    'n_train': n_train,
-    'n_test': n_test,
-    'mse_train': mse_train,
-    'rmse_train': rmse_train,
-    'r2_train': r2_train,
-    'mse_test': mse_test,
-    'rmse_test': rmse_test,
-    'r2_test': r2_test,
-    'y_test_mean': test_mean,
-  }, index=[0])
-
-  summary_df = pd.concat([summary_df_linreg, summary_df_lasso, summary_df_rf, summary_df_rf_2])
+  summary_df = pd.concat([summary_df_linreg, summary_df_lasso, summary_df_rf])
   summary_df.to_csv(f"./player_game_logs/{player_name}/{player_name}_SUMMARY.csv", index=False)
+
+def run_model(model, model_type, X_train, y_train, X_test, y_test, col_names, n_train, n_test, test_mean):
+  y_train_pred = model.predict(X_train)
+  mse_train = mean_squared_error(y_train, y_train_pred)
+  rmse_train = np.sqrt(mse_train)
+  r2_train = r2_score(y_train, y_train_pred)
+
+  y_pred_test = model.predict(X_test)
+  mse_test = mean_squared_error(y_test, y_pred_test)
+  rmse_test = np.sqrt(mse_test)
+  r2_test = r2_score(y_test, y_pred_test)
+  
+  try:
+    # linear models
+    weights = {f"{col}_weight": coef for col, coef in zip(col_names, model.coef_)}
+    bias = model.intercept_
+  except AttributeError:
+    # tree-based models
+    weights = {}
+    bias = ''
+  
+  summary_df = pd.DataFrame({
+    'model_type': model_type,
+    'n_train': n_train,
+    'n_test': n_test,
+    'mse_train': mse_train,
+    'rmse_train': rmse_train,
+    'r2_train': r2_train,
+    'mse_test': mse_test,
+    'rmse_test': rmse_test,
+    'r2_test': r2_test,
+    'y_test_mean': test_mean,
+    'bias': bias,
+  }, index=[0])
+  for key, value in weights.items():
+    summary_df[key] = value
+  
+  return summary_df
 
 def regression_wrapper(player_names):
   for name in player_names:
