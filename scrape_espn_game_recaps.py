37e96a13e8acb83fa1bcc4d1074f39e4f287206e
uncjackg <jackg@live.unc.edu>
2024-04-03 21:15:32 -0400

    try

--- scrape_espn_game_recaps.py
+++ scrape_espn_game_recaps.py
@@ -10,18 +10,21 @@ ESPN_RECAP_URL = "https://www.espn.com/nba/recap/_/gameId/"
 def get_game_recap_urls(dates):
   urls = set()
   for date in dates:
-    print(f"Scraping {date}")
-    driver = init_web_driver()
-    driver.get(ESPN_DATE_URL + date.replace('-', ''))
-    gamecasts = driver.find_elements(By.XPATH, '//a[contains(text(), "Gamecast")]')
-    for g in gamecasts:
-      url_pieces = g.get_attribute('href').split('/')
-      game_id = url_pieces[-2]
-      if game_id != 'gameId':
-        print(game_id)
-        urls.add(ESPN_RECAP_URL + game_id)
-    with open('./espn_game_urls.py', "w") as file:
-      file.write('URLS = ' + str(list(urls)))
+    try:
+      print(f"Scraping {date}")
+      driver = init_web_driver()
+      driver.get(ESPN_DATE_URL + date.replace('-', ''))
+      gamecasts = driver.find_elements(By.XPATH, '//a[contains(text(), "Gamecast")]')
+      for g in gamecasts:
+        url_pieces = g.get_attribute('href').split('/')
+        game_id = url_pieces[-2]
+        if game_id != 'gameId':
+          urls.add(ESPN_RECAP_URL + game_id)
+      with open('./espn_game_urls.py', "w") as file:
+        file.write('URLS = ' + str(list(urls)))
+    except Exception as e:
+      print(e)
+      continue
   
 #dates = generate_dates(start_date=datetime(2002, 10, 29))
 get_game_recap_urls(DATES)
