84eba756570b6e4bc31fdf80e55ecd9de0e5690f
jackgoldsmith4 <jackgoldsmith4@gmail.com>
2024-12-10 09:52:08 -0500

    add log

--- scrape_gamelogs.py
+++ scrape_gamelogs.py
@@ -40,6 +40,7 @@ def scrape_game_log(player_id, player_name, rookie_year, final_year):
     try:
       season_header = driver.find_element(By.XPATH, '//h2[contains(text(), "{}")]/..'.format(season_header_text))
     except:
+      heroku_print(f"WARN: season headers not found for {player_name} in {year}")
       driver.quit()
       continue
     share_export_menu = season_header.find_element(By.CLASS_NAME, 'section_heading_text')
@@ -49,7 +50,7 @@ def scrape_game_log(player_id, player_name, rookie_year, final_year):
     try:
       stats = driver.find_element(By.TAG_NAME, 'pre').text.split(GAMELOG_HEADER_TITLES)[1]
     except:
-      heroku_print('WARN: ' + player_name + ' ' + str(year) + ' logs not found')
+      heroku_print(f"WARN: {player_name} {year} logs not found")
       driver.quit()
       continue
 
